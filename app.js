0650fab28ebf12bdc52c8820d960dad8e2999609
HaruoWang <wang355152@gmail.com>
2023-12-16 14:47:57 +0800

    拿掉芒果

--- app.js
+++ app.js
@@ -9,22 +9,12 @@ app.use(bodyParser.json());
 // 將靜態文件夾指定為 'public'
 app.use(express.static('public'));
 
-app.post('/post', async (req, res) => {
-    try {
-        // 獲取表單數據
-        const { input } = req.body;
+// 處理 POST 請求
+app.post('/process', (req, res) => {
+    const inputData = req.body.data;
 
-        // 創建一個新的文檔
-        const database = new db({ input });
-
-        // 將文檔保存到數據庫
-        await database.save();
-
-        res.json({ message: `Hello, ${input}! Your form is submitted and saved to MongoDB.` });
-    } catch (error) {
-        console.error('Error:', error);
-        res.status(500).json({ error: 'Internal Server Error' });
-    }
+    // 在這裡處理 POST 資料，並回傳相應的回應
+    res.json({ message: `你提交的資料是：${inputData}` });
 });
 
 // 處理 GET 請求
